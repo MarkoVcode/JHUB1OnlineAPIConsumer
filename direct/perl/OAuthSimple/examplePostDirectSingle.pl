b428d84b74248bbd3e27f082205935072fd6f4bb
developer <developer@DevBox.jav.co.uk>
2015-01-10 17:38:03 +0000

    pearl examples

--- direct/perl/OAuthSimple/examplePostDirectSingle.pl
+++ direct/perl/OAuthSimple/examplePostDirectSingle.pl
@@ -5,12 +5,13 @@ use LWP::UserAgent;
 use OAuthSimple;
 
 my $url = "http://192.168.56.30:8189/api/v1/direct/edc_address0/mySensor/23";
+my $key = "edc_this-is-my-user-key";
+my $secret = "edc_this-is-my-user-secret-key";
 
-my $oauth = new OAuthSimple("edc_this-is-my-user-key", "edc_this-is-my-user-secret-key");
+my $oauth = new OAuthSimple($key, $secret);
 $oauth->setAction("POST");
 $oauth->setURL($url);
 my $signElems = $oauth->sign();
-
 
 print(Dumper($signElems));
 
@@ -18,33 +19,10 @@ my $ua = LWP::UserAgent->new;
  
 # set custom HTTP request header fields
 my $req = HTTP::Request->new(POST => $url);
-#$req->header('content-type' => 'application/json');
 $req->header('Authorization' => $signElems->{header});
 
-#print(Dumper($req));
-
- 
-# add POST data to HTTP request body
-#my $post_data = '{ "name": "Dan", "address": "NY" }';
-#$req->content($post_data);
- 
 my $resp = $ua->request($req);
-if ($resp->is_success) {
-    my $message = $resp->decoded_content;
-    print "Received reply: $message\n";
-}
-else {
-    print "HTTP POST error code: ", $resp->code, "\n";
-    print "HTTP POST error message: ", $resp->message, "\n";
-}
 
-
-
-
-
-
-
-#print(Dumper($oauth));
-
-
-#print(Dumper($signElems));
+print "\n---JSON RESPONSE FROM JHUB1 Online---\n\n";
+print $resp->decoded_content;
+print "\n\n-------------------------------------\n";
